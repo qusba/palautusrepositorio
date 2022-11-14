f25f6f4b7eb7e5f7141adae0da6ff3c51c8130e1
qusba <kasper.kahkonen@outlook.com>
2022-11-14 17:42:49 +0200

    poetry-web

---
+++ viikko2/project-reader/src/project_reader.py
@@ -0,0 +1,19 @@
+from urllib import request
+from project import Project
+import toml
+
+
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        # tiedoston merkkijonomuotoinen sisältö
+        content = request.urlopen(self._url).read().decode("utf-8")
+        
+        parsed = toml.loads(content)
+        values = parsed["tool"]["poetry"]
+
+        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
+        return Project(values["name"],values["description"], values["dependencies"], values["dev-dependencies"])

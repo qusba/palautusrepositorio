ff2de6da5fd8319332316461760f7285ef5a9316
qusba <kasper.kahkonen@outlook.com>
2022-11-27 17:54:49 +0200

    Toteutettiin testi kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskorin_hinta_on_sama_kuin_tuotteiden_hintojen_summa ja se meni läpi ilman muutoksia ostoskoriin, lisäksi refaktoroitiin testiluokkaa alustamaan self.maito ja self.leipä

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -5,24 +5,32 @@ from tuote import Tuote
 class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
+        self.maito = Tuote("Maito",3)
+        self.leipä = Tuote("Leipä",5)
 
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
         self.assertEqual(self.kori.tavaroita_korissa(),0)
     
     def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_on_yksi_tuote(self):
-        maito = Tuote("Maito",3)
-        self.kori.lisaa_tuote(maito)
+        
+        self.kori.lisaa_tuote(self.maito)
         self.assertEqual(self.kori.tavaroita_korissa(),1)
 
     def test_yhden_tuotteen_lisaamisen_jalkeen_ostoskorin_hinta_on_sama_kuin_tuotteen_hinta(self):
-        maito = Tuote("Maito",3)
-        self.kori.lisaa_tuote(maito)
+        
+        self.kori.lisaa_tuote(self.maito)
         self.assertEqual(self.kori.hinta(),3)
     
     def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskorissa_on_2_tavaraa(self):
-        maito = Tuote("Maito",3)
-        leipä = Tuote("Leipä",5)
-        self.kori.lisaa_tuote(maito)
-        self.kori.lisaa_tuote(leipä)
+        
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.leipä)
         self.assertEqual(self.kori.tavaroita_korissa(),2)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskorin_hinta_on_sama_kuin_tuotteiden_hintojen_summa(self):
+        
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.leipä)
+        self.assertEqual(self.kori.hinta(),8)
+    
